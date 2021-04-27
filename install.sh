a19a75a4135ed2b47fb19dbef066b4af1a913d2c
Peg0ne <oliverjonasson@gmail.com>
2021-04-27 23:31:00 +0200

    update install again

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ pip3 install PyQt5
 pip3 install pynput
 THISDIR=$(dirname "$0")
 CONFIGPATH="$HOME/.config/ninja"
-BASETEXT="{"colors": {"highlight":"#333333"}, "remember": []}"
+BASETEXT="{\"colors\": {\"highlight\":\"#333333\"}, \"remember\": []}"
 
 mkdir "$CONFIGPATH"
 cat "$CONFIGPATH/ninja.json"
