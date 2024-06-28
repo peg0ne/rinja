76ed08aa8f700e5ff4c6fab194157c0cc5bf1ca6
lazer <lazer@gmail.com>
2024-06-28 11:56:02 +0200

    update install

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ pip3 install PyQt5
 pip3 install pynput
 THISDIR=$(dirname "$0")
 CONFIGPATH="$HOME/.config/rinja"
+BINPATH="$HOME/.local/bin"
 BASETEXT="{\"colors\": {\"highlight\":\"#333333\",\"background\":\"#333333\",\"foreground\":\"#333333\"}, \"rect\": {\"width\": 500, \"height\": 150}, \"remember\": []}"
 
-mkdir "$CONFIGPATH"
+mkdir -p "$CONFIGPATH"
+mkdir -p "$BINPATH"
 cat "$CONFIGPATH/rinja.json"
 echo "$BASETEXT" > "$CONFIGPATH/rinja.json"
 chmod u+x "$THISDIR/rinja.py"
-ln -s "$THISDIR/rinja.py" "$HOME/.local/bin/rinja"
+ln -r -s "$THISDIR/rinja.py" "$BINPATH/rinja"
 
